aea85f0ab44459b16be9912aa75af391aeca7250
ljx619 <x1968317454@163.com>
2023-07-10 19:01:08 +0800

    1.0.2

--- src/FileDemo/CopyFIle.java
+++ src/FileDemo/CopyFIle.java
@@ -5,16 +5,50 @@ import java.io.*;
 
 public class CopyFIle {
     public static void main(String[] args) throws IOException {
-        InputStreamReader isr = new InputStreamReader(new FileInputStream("src\\FileDemo\\CopyAudio.java"));
-        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("copy.java"));
+//        InputStreamReader isr = new InputStreamReader(new FileInputStream("src\\FileDemo\\CopyAudio.java"));
+//        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("copy.java"));
+//        FileReader fileReader = new FileReader("file.txt");
+//        FileWriter fileWriter = new FileWriter("filecopy.txt");
 
-        char[] ch = new char[1024];
-        int len;
-        while((len = isr.read(ch))!=-1){
-            osw.write(ch,0,len);
+//        char[] chs = new char[1024];
+//        int len;
+//        while((len = fileReader.read(chs))!=-1){
+//            fileWriter.write(chs,0,len);
+//        }
+        //
+//        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("file.txt")));
+//        char[] ch = new char[1024];
+//        int len;
+//        while((len = br.read(ch))!=-1){
+//            System.out.println(new String(ch,0,len));
+//        }
+        //BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+        //BufferedWriter bw = new BufferedWriter(new FileWriter("filecopy2.txt"));
+
+        //
+        //char[] ch = new char[1024];
+        //int len;
+       // while((len = br.read(ch))!=-1){
+        //    bw.write(ch,0,len);
+        //}
+
+        //br.close();
+        //bw.close();
+
+
+        BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("filecopy3.txt"));
+        //遍历
+        String st;
+        while((st = br.readLine())!=null){
+            bw.write(st);
+            bw.newLine();
         }
+        br.close();
+        bw.close();
 
-        isr.close();
-        osw.close();
+
+//        fileWriter.close();
+//        fileReader.close();
     }
 }
