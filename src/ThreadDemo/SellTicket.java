7f7f29e33d2b90daef363087256ccf00e91c8209
ljx619 <x1968317454@163.com>
2023-07-12 19:39:08 +0800

    1.0.5

--- src/ThreadDemo/SellTicket.java
+++ src/ThreadDemo/SellTicket.java
@@ -1,16 +1,37 @@
 package ThreadDemo;
 
+import javafx.beans.binding.ObjectExpression;
+
 import java.util.concurrent.TransferQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class SellTicket implements Runnable{
     private int ticket = 100;
+    private Object obj = new Object();
+    private Lock lc = new ReentrantLock();
+
     @Override
-    public void run() {
+    public  void run() {
+
+
         while(true){
-            if(ticket>0){
-                System.out.println(Thread.currentThread().getName()+":"+ticket);
+            lc.lock();
+//            symethod();
+            if (ticket > 0) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + ":" + ticket);
                 ticket--;
             }
+            lc.unlock();
         }
+
+    }
+    private  void symethod(){
+
     }
 }
